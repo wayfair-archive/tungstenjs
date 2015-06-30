27d8358a3d54b87171441b344da8a7898a4dc21e
Matthew DeGennaro <mattdeeg@gmail.com>
2015-06-30 09:03:40 -0400

    Added derived and session properties, using Ampersand syntax as inspiration

--- adaptors/backbone/base_model.js
+++ adaptors/backbone/base_model.js
@@ -4,6 +4,7 @@
 'use strict';
 var Backbone = require('backbone');
 var backboneNested = require('./model_bubbler');
+var _ = require('underscore');
 var hashCollision = require('./hash_collisions');
 /**
  * BaseModel
@@ -12,7 +13,58 @@ var hashCollision = require('./hash_collisions');
  * @class BaseModel
  */
 var BaseModel = Backbone.Model.extend({
-  tungstenModel: true
+  tungstenModel: true,
+
+  /**
+   * Setup derived property listeners
+   */
+  initialize: function() {
+    var derived = _.result(this, 'derived');
+    var relations = _.result(this, 'relations') || {};
+    if (derived) {
+      var self = this;
+      _.each(derived, function (props, name) {
+        // Check if a collection relation is declared
+        var isCollection = false;
+        if (relations[name] && relations[name].tungstenCollection) {
+          isCollection = true;
+        }
+        if (props.fn && props.deps) {
+          var fn = _.bind(props.fn, self);
+          var update = isCollection ? function() {
+            self.getDeep(name).reset(fn());
+          } : function() {
+            self.set(name, fn());
+          };
+          _.each(props.deps, function(dep) {
+            self.listenTo(self, 'change:' + dep, update);
+          });
+          // Sets default value
+          update();
+        }
+      });
+    }
+
+    this.postInitialize();
+  },
+  // Empty default function
+  postInitialize: function() {},
+
+  /**
+   * Derived and session variables should be stripped from data
+   */
+  toJSON: function() {
+    var attrs = this.attributes;
+    var derived = _.result(this, 'derived') || {};
+    var session = _.invert(_.result(this, 'session') || []);
+    var data = {};
+    _.each(attrs, function(val, key) {
+      if (!derived[key] && !session[key]) {
+        data[key] = val;
+      }
+    });
+    return data;
+  }
 }, {
   /**
    * Override of extend to provide basic mixin functionality for colliding hashes
