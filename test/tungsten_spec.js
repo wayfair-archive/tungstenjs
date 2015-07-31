36b82014634366f2eecfadc116fa4412969b8e00
Andrew Rota <rota.andrew@gmail.com>
2015-07-31 07:25:05 -0400

    Switch from jasmine-node to jasmine, add chai, fix createVNode test

--- test/tungsten_spec.js
+++ test/tungsten_spec.js
@@ -3,102 +3,105 @@
  *
  * @author    Andrew Rota <anrota@wayfair.com>
  */
-(function() {
-  /* global describe, it, expect, require */
-  'use strict';
+/* global describe, it, expect, require */
+'use strict';
 
-  // Environment pulls in window and document references from jsdom
-  require('./environment.js');
+// Include Chai assertion library
+var expect = require('chai').expect;
 
-  // Module to test is tungsten.js
-  var tungsten = require('../src/tungsten.js');
+// Module to test is tungsten.js
+var tungsten = require('../src/tungsten.js');
 
-  // VNode required for instanceof checks
-  var VNode = require('virtual-dom/vnode/vnode');
+// VNode required for instanceof checks
+var VNode = require('virtual-dom/vnode/vnode');
 
-  // Start test suite
-  describe('tungsten.js public API', function() {
-    describe('parseDOM', function() {
-      it('should be a function', function() {
-        expect(typeof tungsten.parseDOM).toEqual('function');
+// Start test suite
+describe('tungsten.js public API', function() {
+  describe('parseDOM', function() {
+
+    it('should work', function() {
+      var foo = 'bar';
+      expect(foo).to.have.length(3);
       });
-      it('should return vnode', function() {
-        var el = window.document.createElement('div');
-        el.className = el.className.concat(' js-root');
-        el.innerHTML = '<div class="js-bar"></div>';
-        var rootVEl = tungsten.parseDOM(el, true);
-        expect(rootVEl.tagName).toEqual('DIV');
-        expect(rootVEl.properties).toEqual({className: ' js-root', style: {}});
-        expect(rootVEl.count).toEqual(1);
-        expect(rootVEl instanceof VNode).toBe(true);
-        el = null;
-      });
+
+    it('should be a function', function() {
+      expect(typeof tungsten.parseDOM).to.equal('function');
     });
-    describe('bindEvent', function() {
-      it('should be a function', function() {
-        expect(typeof tungsten.bindEvent).toEqual('function');
-      });
-    });
-    describe('unbindEvent', function() {
-      it('should be a function', function() {
-        expect(typeof tungsten.unbindEvent).toEqual('function');
-      });
-    });
-    describe('toDOM', function() {
-      it('should be a function', function() {
-        expect(typeof tungsten.toDOM).toEqual('function');
-      });
-      it('should return document fragment', function() {
-        var props = {className: ' js-dom', style: {}};
-        var children = [];
-        var vNode = tungsten.createVNode('div', props, children);
-        var dom = tungsten.toDOM(vNode);
-        expect(dom instanceof window.Node).toBe(true);
-        // Should be document fragment (nodeType === 11)
-        expect(dom.nodeType).toEqual(11);
-      });
-    });
-    describe('toString', function() {
-      it('should be a function', function() {
-        expect(typeof tungsten.toString).toEqual('function');
-      });
-      it('should return a string', function() {
-        var props = {className: ' js-dom', style: {}};
-        var children = [];
-        var vNode = tungsten.createVNode('div', props, children);
-        expect(typeof tungsten.toString(vNode)).toEqual('string');
-      });
-      it('should return a DOM string', function() {
-        var props = {className: ' js-dom', style: {}};
-        var children = [];
-        var vNode = tungsten.createVNode('div', props, children);
-        expect(tungsten.toString(vNode)).toEqual('<div class=" js-dom" style=""></div>');
-      });
-      it('should return a DOM string with styles', function() {
-        var props = {className: ' js-dom', style: {color: 'red'}};
-        var children = [];
-        var vNode = tungsten.createVNode('div', props, children);
-        expect(tungsten.toString(vNode)).toEqual('<div class=" js-dom" style="color: red;"></div>');
-      });
-    });
-    describe('createVNode', function() {
-      it('should be a function', function() {
-        expect(typeof tungsten.createVNode).toEqual('function');
-      });
-      it('should return a vnode', function() {
-        var props = {className: ' js-root', style: {}};
-        var children = [];
-        var vNode = tungsten.createVNode('div', props, children);
-        expect(vNode.tagName).toEqual('DIV');
-        expect(vNode.properties).toEqual({className: ' js-root', style: {}});
-        expect(vNode instanceof VNode).toBe(true);
-      });
-    });
-    describe('updateTree', function() {
-      it('should be a function', function() {
-        expect(typeof tungsten.updateTree).toEqual('function');
-      });
+    it('should return vnode', function() {
+      var el = window.document.createElement('div');
+      el.className = el.className.concat(' js-root');
+      el.innerHTML = '<div class="js-bar"></div>';
+      var rootVEl = tungsten.parseDOM(el, true);
+      expect(rootVEl.tagName).to.equal('DIV');
+      expect(rootVEl.properties).to.deep.equal({attributes: {class: ' js-root'}});
+      expect(rootVEl.count).to.equal(1);
+      expect(rootVEl instanceof VNode).to.equal(true);
+      el = null;
     });
   });
-
-}());
+  describe('bindEvent', function() {
+    it('should be a function', function() {
+      expect(typeof tungsten.bindEvent).to.equal('function');
+    });
+  });
+  describe('unbindEvent', function() {
+    it('should be a function', function() {
+      expect(typeof tungsten.unbindEvent).to.equal('function');
+    });
+  });
+  describe('toDOM', function() {
+    it('should be a function', function() {
+      expect(typeof tungsten.toDOM).to.equal('function');
+    });
+    it('should return document fragment', function() {
+      var props = {className: ' js-dom', style: {}};
+      var children = [];
+      var vNode = tungsten.createVNode('div', props, children);
+      var dom = tungsten.toDOM(vNode);
+      expect(dom instanceof window.Node).to.equal(true);
+      // Should be document fragment (nodeType === 11)
+      expect(dom.nodeType).to.equal(11);
+    });
+  });
+  describe('toString', function() {
+    it('should be a function', function() {
+      expect(typeof tungsten.toString).to.equal('function');
+    });
+    it('should return a string', function() {
+      var props = {className: ' js-dom', style: {}};
+      var children = [];
+      var vNode = tungsten.createVNode('div', props, children);
+      expect(typeof tungsten.toString(vNode)).to.equal('string');
+    });
+    it('should return a DOM string', function() {
+      var props = {className: ' js-dom', style: {}};
+      var children = [];
+      var vNode = tungsten.createVNode('div', props, children);
+      expect(tungsten.toString(vNode)).to.equal('<div class=" js-dom" style=""></div>');
+    });
+    it('should return a DOM string with styles', function() {
+      var props = {className: ' js-dom', style: {color: 'red'}};
+      var children = [];
+      var vNode = tungsten.createVNode('div', props, children);
+      expect(tungsten.toString(vNode)).to.equal('<div class=" js-dom" style="color: red;"></div>');
+    });
+  });
+  describe('createVNode', function() {
+    it('should be a function', function() {
+      expect(typeof tungsten.createVNode).to.equal('function');
+    });
+    it('should return a vnode', function() {
+      var props = {className: ' js-root', style: {}};
+      var children = [];
+      var vNode = tungsten.createVNode('div', props, children);
+      expect(vNode.tagName).to.equal('DIV');
+      expect(vNode.properties).to.deep.equal({className: ' js-root', style: {}});
+      expect(vNode instanceof VNode).to.equal(true);
+    });
+  });
+  describe('updateTree', function() {
+    it('should be a function', function() {
+      expect(typeof tungsten.updateTree).to.equal('function');
+    });
+  });
+});
