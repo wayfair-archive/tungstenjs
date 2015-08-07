aa34edc9b5f53aa3ef7a06bb678da22a9f3eb0b5
Andrew Rota <rota.andrew@gmail.com>
2015-08-07 13:57:59 -0400

    Add classOptions (aka classProprerties) back to extend

--- adaptors/backbone/hash_collision.js
+++ adaptors/backbone/hash_collision.js
@@ -1,7 +1,7 @@
 'use strict';
 var _ = require('underscore');
 module.exports = function(originalExtend) {
-  return function(options) {
+  return function(options, classOptions) {
     var proto = this.prototype;
     var keys = _.intersection(_.keys(proto), _.keys(options));
     _.each(keys, function(key) {
@@ -9,6 +9,6 @@ module.exports = function(originalExtend) {
         _.extend(options[key], proto[key]);
       }
     });
-    return originalExtend.call(this, options);
+    return originalExtend.call(this, options, classOptions);
   };
 };
