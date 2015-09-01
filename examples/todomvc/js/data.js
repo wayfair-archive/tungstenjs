b13d4ac5dd37211f561f49a6f86862528d653e3e
MattDeeg <mattdeeg@gmail.com>
2015-09-01 15:49:24 -0400

    Updating data to include filter and corrected logic for footer

--- examples/todomvc/js/data.js
+++ examples/todomvc/js/data.js
@@ -1,8 +1,26 @@
 (function() {
   var data = {
     'todoItems': [{'title': 'lorem ipsum'}, {'title': 'foo'}],
+    'hasTodos': true,
     'todoCount': 2,
-    'todoCountPlural': true
+    'todoCountPlural': true,
+    'filters': [
+      {
+        name: 'All',
+        hash: '',
+        selected: true
+      },
+      {
+        name: 'Active',
+        hash: 'active',
+        selected: false
+      },
+      {
+        name: 'Completed',
+        hash: 'completed',
+        selected: false
+      }
+    ]
   };
   if (typeof window === 'undefined') {
     module.exports = data;
