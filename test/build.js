1d7c9037c78f2a5458190f22e6e8ad9ce05ef2f2
MattDeeg <mattdeeg@gmail.com>
2015-09-03 17:51:14 -0400

    Changing build to only instrument if the --coverage flag is set

--- test/build.js
+++ test/build.js
@@ -5,6 +5,26 @@ var webpack = require('webpack');
  * Build files use non-js extension to avoid linting
  */
 
+var doCoverage = false;
+var args = process.argv;
+for (var i = 0; i < args.length; i++) {
+  if (args[i] === '--coverage') {
+    doCoverage = true;
+    break;
+  }
+}
+
+function getLoader(variable) {
+  if (doCoverage) {
+    return [{
+      test: /\.js(on)?$/,
+      loader: 'istanbul-instrumenter?coverageVar=' + variable
+    }];
+  } else {
+    return [];
+  }
+}
+
 var debugConfig = webpackHelper({
   entry: __dirname + '\\test_target',
   output: {
@@ -15,10 +35,7 @@ var debugConfig = webpackHelper({
     modulesDirectories: ['test/_loaders']
   },
   module: {
-    loaders: [{
-      test: /\.js(on)?$/,
-      loader: 'istanbul-instrumenter?coverageVar=__coverage_debug__'
-    }]
+    loaders: getLoader('__coverage_debug__')
   },
   resolve: {
     alias: {
@@ -36,14 +53,8 @@ var prodConfig = webpackHelper({
   resolveLoader: {
     modulesDirectories: ['test/_loaders']
   },
-  node: {
-    fs: 'empty'
-  },
   module: {
-    loaders: [{
-      test: /\.js(on)?$/,
-      loader: 'istanbul-instrumenter?coverageVar=__coverage_prod__'
-    }]
+    loaders: getLoader('__coverage_prod__')
   },
   resolve: {
     alias: {
