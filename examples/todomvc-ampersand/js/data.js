e3fadc492de0aa35528590f19a90fcd53b0965b7
Andrew Rota <rota.andrew@gmail.com>
2015-09-03 12:34:35 -0400

    Fix ampersand example issues

--- examples/todomvc-ampersand/js/data.js
+++ examples/todomvc-ampersand/js/data.js
@@ -1,8 +1,22 @@
 (function() {
   var data = {
-    'todoItems': [{'title': 'lorem ipsum'}, {'title': 'foo'}],
-    'todoCount': 2,
-    'todoCountPlural': true
+    'filters': [
+      {
+        name: 'All',
+        hash: '',
+        selected: true
+      },
+      {
+        name: 'Active',
+        hash: 'active',
+        selected: false
+      },
+      {
+        name: 'Completed',
+        hash: 'completed',
+        selected: false
+      }
+    ]
   };
   if (typeof window === 'undefined') {
     module.exports = data;
