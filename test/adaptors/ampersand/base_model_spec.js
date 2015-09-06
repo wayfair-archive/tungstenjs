4fad2c23e79a5c9329b53a959966ea4ee02a6626
Andrew Rota <rota.andrew@gmail.com>
2015-09-06 12:21:02 -0400

    Add ampersand model tests

--- test/adaptors/ampersand/base_model_spec.js
+++ test/adaptors/ampersand/base_model_spec.js
@@ -82,3 +82,120 @@ describe('base_model.js constructed api', function() {
   });
   /* develblock:end */
 });
+
+/**
+ * Ampersand model unit tests modified from ampersand-state
+ * https://github.com/AmpersandJS/ampersand-state/blob/master/test/basics.js
+ *    @license MIT
+ *    Copyright © 2014 &yet, LLC and AmpersandJS contributors
+ *    Permission is hereby granted, free of charge, to any person obtaining a
+ *    copy of this software and associated documentation files (the
+ *    "Software"), to deal in the Software without restriction, including
+ *    without limitation the rights to use, copy, modify, merge, publish,
+ *    distribute, sublicense, and/or sell copies of the Software, and to
+ *    permit persons to whom the Software is furnished to do so, subject to
+ *    the following conditions:
+ *    The above copyright notice and this permission notice shall be included
+ *    in all copies or substantial portions of the Software.*
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+describe('base_model.js ampersand functionality', function() {
+  var Person;
+  beforeEach(function() {
+    Person = BaseModel.extend({
+      props: {
+        name: 'string'
+      }
+    });
+  });
+  afterEach(function() {
+    Person = undefined;
+  });
+  it('init with nothing should be okay', function () {
+    // extend requires {} for debug
+    var EmptyModel = BaseModel.extend({});
+    var something = new EmptyModel();
+    something.foo = 'bar';
+    expect(!!something).to.equal(true);
+    expect(something.foo).to.equal('bar');
+  });
+  it('init with values', function () {
+    var person = new Person({name: 'foo'});
+    expect(!!person).to.equal(true);
+    expect(person.name).to.equal('foo');
+  });
+
+  it('after initialized change should be empty until a set op', function () {
+    var person = new Person({name: 'phil'});
+    expect(person._changed).to.deep.equal({});
+    expect(!!person.changedAttributes()).to.equal(false);
+  });
+
+  it('extended object maintains existing props', function () {
+    var AwesomePerson = Person.extend({
+      props: {
+        awesomeness: 'number'
+      }
+    });
+
+    var awesome = new AwesomePerson({
+      name: 'Captain Awesome',
+      awesomeness: 11
+    });
+
+    expect(awesome.name).to.equal('Captain Awesome');
+    expect(awesome.awesomeness).to.equal(11);
+  });
+
+  it('extended object maintains existing methods', function () {
+    var NewPerson = BaseModel.extend({
+      props: {
+        awesomeness: 'number'
+      },
+      isTrulyAwesome: function () {
+        if (this.awesomeness > 10) return true;
+      }
+    });
+    var AwesomePerson = NewPerson.extend({});
+    var awesome = new AwesomePerson({
+      awesomeness: 11
+    });
+    expect(!!awesome.isTrulyAwesome()).to.equal(true);
+  });
+  it('instanceof checks should pass for all parents in the chain', function () {
+    var P1 = Person.extend({});
+    var P2 = P1.extend({});
+    var P3 = P2.extend({});
+    var p1 = new P1();
+    var p2 = new P2();
+    var p3 = new P3();
+    expect(!!(p1 instanceof Person)).to.equal(true);
+    expect(!!(p2 instanceof Person)).to.equal(true);
+    expect(!!(p3 instanceof Person)).to.equal(true);
+    expect(!!(p1 instanceof P2)).to.equal(false);
+    expect(!!(p2 instanceof P2)).to.equal(true);
+    expect(!!(p3 instanceof P2)).to.equal(true);
+    expect(!!(p2 instanceof P3)).to.equal(false);
+    expect(!!(p3 instanceof P3)).to.equal(true);
+
+    // all of them should have the isState flag too
+    expect(!!(p1.isState)).to.equal(true);
+    expect(!!(p2.isState)).to.equal(true);
+    expect(!!(p3.isState)).to.equal(true);
+
+    // shouldn't be possible to change
+    p1.isState = false;
+    p2.isState = false;
+    p3.isState = false;
+    expect(!!(p1.isState)).to.equal(true);
+    expect(!!(p2.isState)).to.equal(true);
+    expect(!!(p3.isState)).to.equal(true);
+
+  });
+});
