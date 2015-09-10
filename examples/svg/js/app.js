b2640bfd6b90ce13c1d2e61c8ab650de5d11bf8f
Matthew DeGennaro <mattdeeg@gmail.com>
2015-09-10 00:20:17 -0400

    Rough SVG rendering examples

---
+++ examples/svg/js/app.js
@@ -0,0 +1,51 @@
+var template = require('../templates/svg_view.mustache');
+
+var TungstenBackboneBase = require('tungstenjs/adaptors/backbone');
+
+var elem = document.getElementById('appwrapper');
+
+var DemoModel = TungstenBackboneBase.Model.extend({
+  relations: {
+    clock: TungstenBackboneBase.Model,
+    chart: TungstenBackboneBase.Model
+  }
+});
+
+var ChartView = TungstenBackboneBase.View.extend({
+  events: {
+    'change .js-city-select': 'changeCity',
+    'change .js-degree-type': 'changeType',
+  },
+  setChart: function() {
+    var index = this.model.get('selectedCityIndex');
+    var type = this.model.get('degreeType');
+    var city = this.model.get('cities')[index];
+    var active = this.model.get('getCity')(city.temperatures, type);
+    this.model.set(active);
+  },
+  changeCity: function(evt) {
+    this.model.set('selectedCityIndex', evt.currentTarget.selectedIndex);
+    this.setChart();
+  },
+  changeType: function(evt) {
+    this.model.set('degreeType', evt.currentTarget.value);
+    this.setChart();
+  }
+});
+var DemoView = TungstenBackboneBase.View.extend({
+  childViews: {
+    'js-chart-view': ChartView
+  }
+});
+
+window.app = module.exports = new DemoView({
+  el: elem,
+  template: template,
+  model: new DemoModel(window.data),
+  dynamicInitialize: elem.childNodes.length === 0
+});
+
+var clockModel = window.app.model.get('clock');
+setInterval(function() {
+  clockModel.set('datetime', new Date());
+}, 1000);
