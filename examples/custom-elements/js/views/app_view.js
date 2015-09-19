def581fabd23c4828e410928a6b311829bf606d7
Andrew Rota <rota.andrew@gmail.com>
2015-09-19 14:44:58 -0400

    Add some example components

--- examples/custom-elements/js/views/app_view.js
+++ examples/custom-elements/js/views/app_view.js
@@ -8,6 +8,15 @@ var View = TungstenBackboneBase.View;
 
 var AppView = View.extend({
   postInitialize: function() {
+    window.addEventListener("WebComponentsReady", function() {
+      var btn = document.getElementById('toggle-collapsible'),
+        collapsible = document.getElementById('collapsible');
+
+      btn.addEventListener('click', function() {
+        collapsible.toggle();
+        btn.setAttribute('aria-expanded', collapsible.opened ? 'true' : 'false');
+      }, false);
+    });
     console.log('app view initialized');
   }
 }, {
--- examples/custom-elements/readme.md
+++ examples/custom-elements/readme.md
@@ -1,1 +1,3 @@
 # Tungsten.js + Custom Elements
+
+Examples using [Bosonic](http://bosonic.github.io/index.html) web components
