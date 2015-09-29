fc0709adf602477cd49a89e9681fa1a7f395923e
Andrew Rota <rota.andrew@gmail.com>
2015-09-29 16:20:18 -0400

    Set version from file, not entire package.json

--- webpack-helper.js
+++ webpack-helper.js
@@ -50,6 +50,7 @@ module.exports = function(config, dev) {
     ensureLoader(config.module.preLoaders, /\.js$/, 'webpack-strip-block');
   }
   ensureLoader(config.module.loaders, /\.json$/, 'json-loader');
+  ensureLoader(config.module.loaders, /VERSION$/, 'raw-loader');
   ensureLoader(config.module.loaders, /\.mustache$/, 'tungsten_template');
 
   return config;
