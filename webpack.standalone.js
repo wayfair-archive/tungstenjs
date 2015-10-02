6ec208fcf4125d3a4992127cb4a2ec1b2f8f1a3d
Andrew Rota <rota.andrew@gmail.com>
2015-10-02 14:51:08 -0400

    Build both adaptors

--- webpack.standalone.js
+++ webpack.standalone.js
@@ -1,23 +1,11 @@
-// to build: webpack --config webpack.standalone.js --backbone
 var path = require('path');
 var webpack = require('webpack');
 var webpackSettings = require('./webpack-helper');
-var adaptorName = '';
-process.argv.forEach(function (val) {
-  switch(val) {
-    case '--ampersand':
-    case '--backbone':
-      adaptorName = val.substr(2);
-      break;
-  }
-});
-if(!adaptorName) {
-  throw('Must declare an adaptor option');
-}
-console.log('Building tungsten.js + ' + adaptorName + ' adaptor');
+
 module.exports = webpackSettings({
   entry: {
-    'adaptor': './adaptors/' + adaptorName,
+    'backbone': './adaptors/backbone',
+    'ampersand': './adaptors/ampersand',
     'core': './tungsten',
     'template': ['./src/template/template.js']
   },
@@ -40,6 +28,6 @@ module.exports = webpackSettings({
     loaders: []
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('./dist/tungsten.adaptor.js', './dist/tungsten.core.js')
+    new webpack.optimize.CommonsChunkPlugin('./dist/tungsten.core.js')
   ]
 });
