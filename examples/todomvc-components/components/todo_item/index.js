230bfbf0c3fb8cc42be923419a76b7a558a95789
MattDeeg <mattdeeg@gmail.com>
2015-11-30 13:28:16 -0500

    Updating for component changes

--- examples/todomvc-components/components/todo_item/index.js
+++ examples/todomvc-components/components/todo_item/index.js
@@ -10,7 +10,6 @@ module.exports = function(data) {
   if (data && data.constructor === ComponentWidget) {
     return data;
   }
-  var id = _.uniqueId('w_subview');
   var model = new Model(data);
-  return new ComponentWidget(View, model, template, id);
+  return new ComponentWidget(View, model, template);
 };
--- examples/todomvc-components/components/todo_item/model.js
+++ examples/todomvc-components/components/todo_item/model.js
@@ -5,6 +5,7 @@
 
 var Model = require('tungstenjs/adaptors/backbone').Model;
 var ItemModel = Model.extend({
+  exposedEvents: ['change:completed'],
   toggle: function() {
     this.set({
       completed: !this.get('completed')
