d86b087cb9246c9a0fc1063262a5538fe4637b19
Andrew Rota <rota.andrew@gmail.com>
2015-12-07 20:09:34 -0500

    Add selected states

--- tutorials/js/app.js
+++ tutorials/js/app.js
@@ -1,11 +1,10 @@
 var rawTemplates = {
-  app: '<div id="menu"> <div class="pure-menu"><a class="pure-menu-heading" href="#">Tungsten.js</a> <ul class="pure-menu-list">{{#tutorials}} <li class="pure-menu-item"><a href="#" class="pure-menu-link js-tutorial-select">{{name}}</a></li> {{/tutorials}} </ul> </div> </div> <div class="pure-g js-main"> {{#tutorial}} <div class="pure-u-1-2 page-section description"> <ol> {{#steps}} <li class="js-step-select step">{{index}}</li> {{/steps}} </ol> {{#step}} <p>{{description}}</p> <button class="pure-button js-run" id="run">Run</button> {{/step}} </div> {{#step}} <div class="pure-u-1-2 page-section"><textarea class="js-editor editor html" cols="30" rows="25" id="template" value="{{template}}">{{template}}</textarea></div> <div class="pure-u-1-2 page-section"> <div id="result"> <div id="app"></div> </div> </div> <div class="pure-u-1-2 page-section"><textarea class="js-editor editor" id="tungsten" cols="30" rows="25" value="{{js}}">{{js}}</textarea>{ </div> {{/step}} {{/tutorial}} </div>'
+  app: '<div id="menu"> <div class="pure-menu"><a class="pure-menu-heading" href="#">Tungsten.js</a> <ul class="pure-menu-list">{{#tutorials}} <li class="pure-menu-item"><a href="#" class="pure-menu-link js-tutorial-select" {{#selected}}style="background: #333;"{{/selected}}>{{name}}</a></li> {{/tutorials}} </ul> </div> </div> <div class="pure-g js-main"> {{#tutorial}} <div class="pure-u-1-2 page-section description"> <ol class="steps"> {{#steps}} <li class="js-step-select step" {{#selected}}style="font-weight: bold;"{{/selected}}>{{index}}</li> {{/steps}} </ol> {{#step}} <p>{{description}}</p> <button class="pure-button js-run" id="run">Run</button> {{/step}} </div> {{#step}} <div class="pure-u-1-2 page-section"><textarea class="js-editor editor html" cols="30" rows="25" id="template" value="{{template}}">{{template}}</textarea></div> <div class="pure-u-1-2 page-section"> <div id="result"> <div id="app"></div> </div> </div> <div class="pure-u-1-2 page-section"><textarea class="js-editor editor" id="tungsten" cols="30" rows="25" value="{{js}}">{{js}}</textarea></div> {{/step}} {{/tutorial}} </div>'
 };
 
 var tungstenCode;
 var templateCode;
 
-// Compile templates; usually this is done at build time.
 var compiledTemplates = tungsten.template.compileTemplates(rawTemplates);
 
 var View = tungsten.backbone.View, Model = tungsten.backbone.Model, Collection = tungsten.backbone.Collection;
@@ -86,8 +85,20 @@ var AppView = View.extend({
       }
       self.model.unset('step');
       self.model.unset('tutorial');
+
+      self.model.get('tutorials').each(function(tutorials) {
+        tutorials.set('selected', false);
+      });
+      tutorial.set('selected', true);
+
       self.model.set('tutorial', tutorial.toJSON());
       self.listenTo(self.model.get('tutorial').get('steps'), 'selectStep', function(s) {
+
+
+        self.model.get('tutorial').get('steps').each(function(step) {
+          step.set('selected', false);
+        });
+        s.set('selected', true);
         self.model.get('tutorial').set('step', s);
         _.each(document.querySelectorAll('.CodeMirror'), function(editor) {
           editor.remove();
@@ -136,7 +147,13 @@ var AppView = View.extend({
 });
 var TutorialModel = Model.extend({
   relations: {
-    steps: Collection
+    steps: Collection.extend({
+      model: Model.extend({
+        defaults: {
+          selected: false
+        }
+      })
+    })
   }
 });
 var TutorialCollection = Collection.extend({
