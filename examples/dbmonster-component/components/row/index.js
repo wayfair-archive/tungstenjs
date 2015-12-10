c5a16fb8222e763c0fc7d41f88dcce5456686951
MattDeeg <mattdeeg@gmail.com>
2015-12-10 16:40:04 -0500

    Adding componentized version of dbmon

---
+++ examples/dbmonster-component/components/row/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var ComponentWidget = require('tungstenjs/adaptors/backbone').ComponentWidget;
+
+var Model = require('./model');
+var View = require('./view');
+var template = require('./template.mustache');
+
+module.exports = function(data, options) {
+  if (data && data.constructor === ComponentWidget) {
+    return data;
+  }
+  return new ComponentWidget(View, new Model(data), template, options);
+};
+
+module.exports.prototype = {
+  idAttribute: Model.prototype.idAttribute
+};
---
+++ examples/dbmonster-component/readme.md
@@ -0,0 +1,10 @@
+# Tungsten.js DBMonster Example
+
+Example implementation of Ryan Florance's [DBMonster](https://github.com/ryanflorence/reactconf-2015-HYPE/tree/master/demos/01-dbmon) demo app written with Tungsten.js and Backbone.js.
+
+## Run
+
+    # Run `npm install` on the main tungstenjs directory
+    npm install
+    npm start
+    # To run in debug mode: `npm start -- --dev`
