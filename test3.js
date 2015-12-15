c51ed7a4d4e1e130de47a14651a96fb3d6c5e121
MattDeeg <mattdeeg@gmail.com>
2015-12-15 00:13:31 -0500

    Updating test file

--- test3.js
+++ test3.js
@@ -2,16 +2,19 @@
 require('./test/environment');
 var fs = require('fs');
 var template = `{{! w/test }}
-<div selected class="{{class}}" {{#a}}{{#b}}data-{{c}}-a="bar"{{/b}} data-bar="{{foo}}"{{/a}} {{{test}}}>
+<div selected="selected" class="{{class}}" {{#a}}{{#b}}data-foo-a="bar"{{/b}} data-bar="{{foo}}"{{/a}} {{{test}}}>
   {{#test}}fa<span>ff</span>ce{{/test}}
   <!-- t{{#b}}es{{/b}}t-->
   {{> my/partial/name }}
   {{^test}}book{{/test}}
 </div>`;
 
-template = `<div>1<div>23</div></div>`;
+// template = `<div>1<div>23</div></div>`;
 // template = fs.readFileSync('/Users/deeg/Projects/tungstenjs/examples/svg/templates/chart_view.mustache').toString();
 
 var compiler = require('./src/template/compiler');
-var r1 = JSON.stringify(compiler(template), null, '  ');
-console.log(r1);
+var reverse = require('./src/template/ractive_adaptor');
+var tmpl = compiler(template).template;
+var str = tmpl.toSource();
+console.log(template);
+console.log(str);
