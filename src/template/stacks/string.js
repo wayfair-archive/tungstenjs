b46d13b5b7e812863a174943d1a71aca8ba2dd01
MattDeeg <mattdeeg@gmail.com>
2015-12-30 10:38:45 -0500

    Added decoding to the string parser for entities within attribute values

--- src/template/stacks/string.js
+++ src/template/stacks/string.js
@@ -1,12 +1,26 @@
 'use strict';
 
 var DefaultStack = require('./default');
+var htmlParser = require('../html_parser');
 
 function StringStack(attributesOnly, debugMode) {
   DefaultStack.call(this, attributesOnly, debugMode);
 }
 StringStack.prototype = new DefaultStack();
 StringStack.prototype.constructor = StringStack;
+
+StringStack.prototype.createObject = function(obj, options) {
+  if (typeof obj === 'string' && options && options.parse) {
+    // Naive check to avoid parsing if value contains nothing HTML-ish or HTML-entity-ish
+    if (obj.indexOf('<') > -1 || obj.indexOf('&') > -1) {
+      htmlParser(obj, this);
+    } else {
+      this._closeElem(obj);
+    }
+  } else {
+    this._closeElem(obj);
+  }
+};
 
 StringStack.prototype.openElement = function() {
   throw 'An attribute cannot contain an element';
