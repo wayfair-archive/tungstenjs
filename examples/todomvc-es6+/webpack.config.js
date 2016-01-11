582e19cdf772d828e2681e8fcf823345d884e78e
Andrew Rota <rota.andrew@gmail.com>
2016-01-10 20:58:03 -0500

    Update example to babel 6.x

--- examples/todomvc-es6+/webpack.config.js
+++ examples/todomvc-es6+/webpack.config.js
@@ -4,7 +4,7 @@ var config = require('../base_webpack.config.js')(__dirname);
 config.resolveLoader.root = path.join(__dirname, 'node_modules');
 config.module.loaders.unshift({
   test: /\.js$/,
-  loader: 'babel', exclude: /node_modules/
+  loader: 'babel?stage=0', exclude: /node_modules/
 });
 
 module.exports = config;
