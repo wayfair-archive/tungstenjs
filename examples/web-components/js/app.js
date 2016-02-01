e06fafc5e15705b03cb3f242d1b129b427ac6f96
Andrew Rota <rota.andrew@gmail.com>
2016-02-01 08:36:16 -0500

    Fix web components example init

--- examples/web-components/js/app.js
+++ examples/web-components/js/app.js
@@ -3,7 +3,7 @@ var AppModel = require('./models/app_model');
 var template = window.template = require('../templates/app_view.mustache');
 
 module.exports = window.view = new AppView({
-  el: '#appwrapper',
+  el: document.getElementById('appwrapper'),
   template: template,
   model: new AppModel(window.data)
 });
