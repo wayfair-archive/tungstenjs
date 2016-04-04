3139788200821a01e7a16a935545ec51a340a3ee
Brian LaMattina <blamattina@users.noreply.github.com>
2016-04-04 08:56:09 -0400

    Debugger Function Instrumentation
    * Add browser and node specific build targets
    
    * Instrument tracked functions in debugger
    
    * Remove unused RegistryWrapper dependency
    
    * Specs for instrumentFunction
    
    * Fix build issues
    
    * 0.12.0

---
+++ src/debug/instrument_function.js
@@ -0,0 +1,35 @@
+const Timer = require('./timer');
+const logger = require('../utils/logger');
+
+/*
+ * Instrument a function and log the following when it is called:
+ *
+ *  1. Arguments
+ *  2. Return Value
+ *  3. Execution Time
+ *
+ *  @param {String} fnName Function name
+ *  @param {Function} fn Function
+ *  @returns {Function} Instrumented Function
+ *
+ *  @example
+ *  var myFunc = function() { return 4; };
+ *  var myFuncInstrumented = instrumentedFunction('myFunc', myFunc);
+ *
+ *  myFuncInstrumented(1, 2, 3);
+ *
+ *  => "myFunc" called with arguments: [1, 2, 3]
+ *  => "myFunc" executed in 2.0034ms and returned: 4
+ */
+module.exports = function instrumentFunction(fnName, fn) {
+  return function instrumentedFunction() {
+    logger.trace(`${fnName} called with arguments: `, arguments);
+
+    const timer = new Timer(fnName, false);
+    const returnValue = fn.apply(this, arguments);
+    const measurement = parseFloat(timer.getMeasurement()).toFixed(4);
+
+    logger.trace(`${fnName} executed in ${measurement}ms and returned: `, returnValue);
+    return returnValue;
+  };
+};
