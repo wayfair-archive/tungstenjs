b5ae7707eb201d3cca67af86a97e804a1d98c365
Artem Ruts <aruts@wayfair.com>
2016-05-19 12:33:19 -0400

    Making sure we don't break other computed props

--- adaptors/backbone/context_adaptor.js
+++ adaptors/backbone/context_adaptor.js
@@ -65,7 +65,7 @@ var lookupValue = function(view, name) {
     return view.at(name);
   }
   // Fall back to a property check
-  if (view.hasOwnProperty(name)) {
+  if (view[name] != null && typeof view === 'object') {
     if (view.tungstenCollection && blockedCollectionProperties[name]) {
       return null;
     }
