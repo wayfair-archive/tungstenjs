85fe2f2215f38b133aab5ea2aecd10b8fb0948c8
Kofi Owusu Acheampong <kowusuacheampong@wayfair.com>
2016-07-05 13:38:22 -0400

    child view selector check

--- adaptors/shared/validate.js
+++ adaptors/shared/validate.js
@@ -1,6 +1,11 @@
 'use strict';
 
 module.exports.childViews = function(childViews) {
+
+  if('el' in childViews) {
+    throw new Error('Child views cannot contain property "el"');
+  }
+
   for (var selector in childViews) {
     if (selector.substr(0, 4) === '.js-') {
       throw new Error('Child views cannot start with a period: "' + selector + '"');
