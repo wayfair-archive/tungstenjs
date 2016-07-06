5a9af12667922161e4bd40767b5f874d53d2ffc3
Kofi Owusu Acheampong <kowusuacheampong@wayfair.com>
2016-07-06 16:11:09 -0400

    throw exception for existence of el property on child views

--- adaptors/shared/validate.js
+++ adaptors/shared/validate.js
@@ -1,12 +1,10 @@
 'use strict';
 
 module.exports.childViews = function(childViews) {
-
-  if('el' in childViews) {
-    throw new Error('Child views cannot contain property "el"');
-  }
-
   for (var selector in childViews) {
+    if (Object.keys(childViews[selector].prototype).indexOf('el') !== -1) {
+      throw new Error('Child views cannot contain property "el"');
+    }
     if (selector.substr(0, 4) === '.js-') {
       throw new Error('Child views cannot start with a period: "' + selector + '"');
     } else if (selector.substr(0, 3) !== 'js-') {
