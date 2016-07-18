469ac209a345dd1e8246697df8c14aaf6cd1b45f
MattDeeg <mattdeeg@gmail.com>
2016-07-18 12:48:24 -0400

    Stop loading window_manager during unit tests as it seems to cause some glorious side effects

--- src/debug/index.js
+++ src/debug/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
-module.exports = {
-  registry: require('./registry'),
-  panel: require('./window_manager'),
-  vtreeToString: require('./vtree_to_string'),
-  diffVtreeAndElem: require('./diff_dom_and_vdom'),
-  diff: require('./text_diff'),
-  timer: require('./timer')
-};
+var exports = {};
+exports.registry = require('./registry');
+exports.vtreeToString = require('./vtree_to_string');
+exports.diffVtreeAndElem = require('./diff_dom_and_vdom');
+exports.diff = require('./text_diff');
+exports.timer = require('./timer');
+
+if (typeof TUNGSTENJS_IS_TEST === 'undefined') {
+  // Window manager does something that messes with the unit tests and is unnecessary to include
+  exports.panel = require('./window_manager');
+}
+
+module.exports = exports;
