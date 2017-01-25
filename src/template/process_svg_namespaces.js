503c47904f8b5cec933d762f8e37a25359b8888d
Matt DeGennaro <mdegennaro@wayfair.com>
2017-01-25 12:00:02 -0500

    Fixing svg attributes hooks

--- src/template/process_svg_namespaces.js
+++ src/template/process_svg_namespaces.js
@@ -1,20 +1,28 @@
+const _ = require('underscore');
 const svgAttributeNamespace = require('virtual-dom/virtual-hyperscript/svg-attribute-namespace');
 const AttributeHook = require('virtual-dom/virtual-hyperscript/hooks/attribute-hook');
 
-module.exports = function processSvgNamespace(attributes) {
-  if (!attributes) {
-    return attributes;
+module.exports = function processSvgNamespace(properties) {
+  if (!properties || !properties.attributes) {
+    return properties;
   }
 
-  let namespaced = {};
+  let namespacedAttrs = {};
+  let props = _.create(properties);
+  props.attributes = namespacedAttrs;
+
+  let attributes = properties.attributes;
   for (let attrName in attributes) {
-    let namespace = svgAttributeNamespace(attrName);
-    if (namespace) {
-      namespaced[attrName] = new AttributeHook(namespace, attributes[attrName]);
-    } else {
-      namespaced[attrName] = attributes[attrName];
+    if (attrName !== 'namespace') {
+      let namespace = svgAttributeNamespace(attrName);
+      if (namespace) {
+        // Hooks need to be moved to the top object to be processed
+        props[attrName] = new AttributeHook(namespace, attributes[attrName]);
+      } else {
+        namespacedAttrs[attrName] = attributes[attrName];
+      }
     }
   }
 
-  return namespaced;
+  return props;
 };
